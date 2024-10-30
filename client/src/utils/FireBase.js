366a3910e221b82e2682546fc529d8a5acc9b7d2
Adarsh Goutam <adarsh47111@gmail.com>
2024-10-30 07:56:40 +0530

    addded firebase keys to .env

--- client/src/utils/FireBase.js
+++ client/src/utils/FireBase.js
@@ -1,31 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import {
   getDownloadURL,
   getStorage,
   ref,
-  uploadBytes,
   uploadBytesResumable,
 } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDUEoJYTqpWsTkcXXT3KFzd-x5yxQZH59E",
-  authDomain: "chat-app-690d2.firebaseapp.com",
-  projectId: "chat-app-690d2",
-  storageBucket: "chat-app-690d2.appspot.com",
-  messagingSenderId: "422240242400",
-  appId: "1:422240242400:web:495931c7277117916ede85",
-  measurementId: "G-TBHWH0BR43",
+  apiKey: import.meta.env.VITE_FIREBASE_apiKey,
+  authDomain: import.meta.env.VITE_FIREBASE_authDomain,
+  projectId: import.meta.env.VITE_FIREBASE_projectId,
+  storageBucket: import.meta.env.VITE_FIREBASE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_messagingSenderId,
+  appId: import.meta.env.VITE_FIREBASE_appId,
+  measurementId: import.meta.env.VITE_FIREBASE_measurementId,
 };
 
 // Initialize Firebase
 export const appFirebase = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(appFirebase);
 
 export const uploadFile = ({ path, fileName, file }) => {
   return new Promise((resolve, reject) => {
